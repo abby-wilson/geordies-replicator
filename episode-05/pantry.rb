d5392992f6c036440d95a4e2030ca205f1d3db65
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-08 14:09:58 -0400

    episode 9 debugged

---
+++ episode-05/pantry.rb
@@ -0,0 +1,23 @@
+class Pantry
+require 'pry'
+  attr_accessor :shelf
+
+  def initialize
+    @shelf = Location.new
+    stock_shelf
+  end
+
+  def stock_shelf
+    Ingredient.names.each do |ingredient_name|
+      3.times do
+        @shelf.contents << Ingredient.new(ingredient_name)
+      end
+    end
+  end
+
+  def find_ingredient(name)
+    # binding.pry
+    @shelf.contents.find { |item| item.name == name }
+  end
+
+end
